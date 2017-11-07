b66b0b634d672c97cd73765fb0ca4c8a07b816ce
Nikolai Yakuschenko <nick22783@gmail.com>
2017-11-07 23:29:51 +0300

    temperature assings to city correctly

--- js/jquery.temperature.js
+++ js/jquery.temperature.js
@@ -3,69 +3,36 @@
     // Default settings
     var settings = $.extend({
       'defaultCities' : ['Saint Petersburg', 'Moscow', 'Paris', 'London', 'Berlin',
-      'Barcelona', 'New York', 'Ottawa', 'Canberra', 'Wellington'],
+      'Barcelona', 'New York', 'Ottawa', 'Canberra', 'Wellington', 'Norilsk'],
       'move': 'up'
     }, options);
 
     return this.each(function() {
     // Getting the temperature via AJAX
-    //--------------------------------------
-    // var searchedCity = 'Los Angeles';
-    var searchedCity;
-    //----------------------------------------
-    var temperature;
-    // function handleData(data) {
-    // //   temperature = responseData;
-    // //   console.log('temperature = ' + temperature);
-    //  var temp = Math.round(data.main.temp) +
-    //         '&#176; in ' + data.name + ', by the way.';
-    //       $('.degree').html(temp);
-    // }
-      function getWeather(city) {
-        console.log('city = ' + city);
-        var cities = [];
-      $.ajax({url: 'http://api.openweathermap.org/data/2.5/weather?q=' +
-        city +
-        '&units=metric&id=524901&APPID=34b222875f6018c8fdd6442a6aac5056',
+    function getWeather(city, id) {
+      var ajaxCall = $.ajax({url: 'http://api.openweathermap.org/data/2.5/weather?q=' +
+        city + '&units=metric&id=524901&APPID=34b222875f6018c8fdd6442a6aac5056',
         dataType: 'json',
-        success: function(data){
-          // handleData(data);
-          // var city = data.name;
-          // var city = searchedCity;
-
-          var temp = Math.round(data.main.temp) +
-            '&#176; in ' + city + ', by the way.';
-          $('.degree').html(temp);
-
-          // console.log('city = ' + city);
-          // var cities = settings.defaultCities;
-        },
         error: function() {
           console.log('Error with Ajax!');
         }
       });// END OF $.ajax
+
+      ajaxCall.done(function(data) {
+        var temp = ' ' + Math.round(data.main.temp) +
+        '&#176; in ' + data.name + ', by the way.';
+        $('#' + id).html(temp);
+      });
     }// END OF getWeather()
 
     // Appending temperature to li containing city
     var mentionedCities = settings.defaultCities;
-    var matchedCities = [];
-    // var temperature = '11';
     mentionedCities.forEach(function(city, i){
-      // var searchedCity = city;
-      matchedCities[i] = city;
-
-
-      // var stringToAppend = '<span class="degree"> ' + temperature +
-      // '&#176; in ' + city + ', by the way.</span>';
       var stringToAppend = '<span class="degree" id=' + i + '></span>';
-      var liWithCity = '> li:contains(' + matchedCities[i] + ')';
+      var liWithCity = '> li:contains(' + city + ')';
       $(liWithCity, this).append(stringToAppend);
-      // console.log('liWithCity = ' + liWithCity);
-      getWeather(matchedCities[i]);
-
+      getWeather(city, i);
     }, this);
-
-    console.log('matchedCities[] = ' + matchedCities);
 
     // Moving li by click
     var error = '<div class="error"> Error! Please enter "{\'move\': \'up\'}" or "{\'move\': \'down\'}" as second parameter to define direction of moving "li" when clicked!</div>';
@@ -82,9 +49,6 @@
       };
     }
     $('> li', this).on('click', move(settings.move));
-
-
-
 
   });// END OF return this.each(function()
 };// END OF $.fn.temperature = function(options)
